92f65c4e04acc4626bb84f05f5b39633baf6fc97
yassineelhoubi <elhoubiyassine@gmail.com>
2022-04-02 11:55:09 +0000

    setup tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,7 @@
-import '../styles/globals.css'
+
 import type { AppProps } from 'next/app'
 
+import "../assets/styles/globals.css"
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
