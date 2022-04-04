ea16f29425b72a47e789028eafea2c76d73cd15c
YounessHassoune <yhassoune7@gmail.com>
2022-04-04 15:03:13 +0100

    setup apollo client

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,26 @@
-import type { NextPage } from 'next'
+import type { GetServerSideProps, NextPage } from 'next'
+import apolloClient from '../graphql/apolloClient';
+import { GetAllDocument } from '../graphql/generated';
 
-const Home: NextPage = () => {
+const Home: NextPage = (data:any) => {
+  console.log(data);
+  
   return (
     <div className="text-5xl">
-      <h1>Mega store app</h1>
-      </div>
+      <h1>{data.data}</h1>
+    </div>
   )
 }
+export const getServerSideProps: GetServerSideProps = async () => {
+  const { data } = await apolloClient.query({
+    query: GetAllDocument,
+  });
+
+  return {
+    props: {
+      data: data?.getAll,
+    },
+  };
+};
 
 export default Home
