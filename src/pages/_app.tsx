d9943232825233d8952f4f0f6ef8ee3c721c9cad
Rafikyoucode <rafikcoding@gmail.com>
2022-04-11 15:03:41 +0000

    add nested layouts

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,10 +3,23 @@ import { ApolloProvider } from '@apollo/client'
 import type { AppProps } from 'next/app'
 import "../assets/styles/globals.css"
 import apolloClient from '../graphql/apolloClient'
-function MyApp({ Component, pageProps }: AppProps) {
+
+type ComponentWithPageLayout = AppProps & {
+  Component: AppProps["Component"] & {
+    PageLayout?: React.ComponentType;
+  };
+};
+function MyApp({ Component, pageProps }: ComponentWithPageLayout) {
   return (
     <ApolloProvider client={apolloClient}>
-    <Component {...pageProps} />
+
+{Component.PageLayout ? (
+        <Component.PageLayout>
+          <Component {...pageProps} />
+        </Component.PageLayout>
+      ) : (
+        <Component {...pageProps} />
+      )}
     </ApolloProvider>
   )
 }
